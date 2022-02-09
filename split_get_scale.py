71d56bb922a97271c2262223e2b929fd316e1640
scottschmidl <sjschmidl@gmail.com>
2022-02-09 13:32:41 -0600

    scotts nb: finished viz and stats, scotts_modeling nb: finished, sgs: edits

--- split_get_scale.py
+++ split_get_scale.py
@@ -8,31 +8,25 @@ class SplitGetScale:
 
     def split(self, df):
 
-        train, test = train_test_split(df, test_size=.15, random_state=123)
-        train, validate = train_test_split(train, test_size=.2, random_state=123)
+        train, test = train_test_split(df, test_size=.2, random_state=123)
 
-        return train, validate, test
+        return train, test
 
-    def get_Xy(self, train, validate, test, cols_drop=None):
+    def get_Xy(self, train, test, cols_train=None):
 
-        if cols_drop:
-            X_train = train.drop(cols_drop, axis=1)
-            X_val = validate.drop(cols_drop, axis=1)
-            X_test = test.drop(cols_drop, axis=1)
+        if cols_train:
+            X_train = train[cols_train]
+            X_test = test[cols_train]
 
-        tfid = TfidfVectorizer()
+        tfidf = TfidfVectorizer()
 
-        X_train =
-        X_val =
-        X_test =
+        X_train = tfidf.fit_transform(X_train)
+        X_test = tfidf.transform(X_test)
 
         y_train = train["language"]
-        y_val = validate["language"]
         y_test = test["language"]
 
-
-
-        return (X_train, y_train), (X_val, y_val), (X_test, y_test)
+        return (X_train, y_train), (X_test, y_test)
 
     def scale(self, X_train, X_validate, X_test):
 
@@ -40,7 +34,6 @@ class SplitGetScale:
         scale.fit(X_train)
 
         X_train_scaled = pd.DataFrame(data=scale.transform(X_train), columns=X_train.columns)
-        X_val_scaled = pd.DataFrame(data=scale.transform(X_validate), columns=X_train.columns)
         X_test_scaled = pd.DataFrame(data=scale.transform(X_test), columns=X_train.columns)
 
-        return X_train_scaled, X_val_scaled, X_test_scaled, scale
+        return X_train_scaled, X_test_scaled, scale
